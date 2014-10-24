b8a2bcff64ae0844dc033a4935f8499591e8f751
Patrick Hidalgo <m.patrick.hidalgo@gmail.com>
2014-10-24 08:36:58 -0600

    last lrthw

---
+++ chapter_10/Readme.md
@@ -0,0 +1,10 @@
+* 1)  List two or three escape sequences in Readme.md, along with what they do
+
+> \\ is an escape sequence for a backslash.
+> \' is an escape sequence for a single quote.
+> \'' is an escape sequence for a double quote.
+> \n is an escape sequence for a line feed.
+
+* 2)  Add this as a final line in ex10.rb
+
+
---
+++ chapter_10/ex10.rb
@@ -0,0 +1,24 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat  = "I'm split \non line."
+backslash_cat = "I'm \\ a \\ cat."
+#
+# fat_cat = """
+# I'll do a list
+# \t* Cat food
+# \t* Fishies
+# \t* Catnip\n\t* Grass
+# """
+
+fat_cat = '''
+I\'ll do a list
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+'''
+puts tabby_cat
+puts persian_cat
+puts backslash_cat
+puts fat_cat
+
+# Triple single quotes don't allow you to do a ton of formatting
+# unless you go overboard with the quotes.
